0d0c8228724c3fa0726340b67173e642006af7e0
Beatryz da Silva Pina <beatryzpina@gmail.com>
2025-05-01 16:53:29 -0300

    first commit

---
+++ portifólio/temperatura.c
@@ -0,0 +1,19 @@
+#include <iostream>
+     void recebeTemperatura ()
+{
+    float temperatura [8];
+    for (int i = 0; i < 8 ; i ++){
+
+        printf("Digite a temperatura as %dh: ", i+9);
+        scanf("%f", &temperatura[0]);
+    }
+
+}
+
+
+int main()
+{
+   recebeTemperatura();
+
+    return 0;
+}
