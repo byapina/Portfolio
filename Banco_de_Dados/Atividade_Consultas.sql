4c7e0cb7a81bbf849bf1a0b2ae6a90231d5ceedc
Beatryz da Silva Pina <beatryzpina@gmail.com>
2025-05-24 13:54:16 -0300

    Update Atividade_Consultas.sql

--- Banco_de_Dados/Atividade_Consultas.sql
+++ Banco_de_Dados/Atividade_Consultas.sql
@@ -13,6 +13,7 @@ departamento_dnumero int,
 foreign key (supervisor_cpf) references Funcionario(cpf)
 );
 
+
 alter table Funcionario add constraint fk_categorie foreign key (departamento_dnumero) references Departamento(dnumero);
 
 create table Departamento (
@@ -106,14 +107,61 @@ from funcionario as f
 where (
 select count(*)
 from dependente as d
-where d.f_cpf = f.cpf) >= 2
+where d.f_cpf = f.cpf) >= 2;
 
 -- 12
+select pnome
+from Funcionario as f
+where exists (
+	select *
+	from Departamento as d
+	where D.cpf_gerente = F.cpf
+) 
+and 
+exists ( 
+	select *
+	from Dependente as dep
+	where DEP.f_cpf = F.cpf
+);
 
+-- 13
+select pnome
+from funcionarios as f
+where exists (
+	select *
+	from trabalha_em as te
+	inner join prjeto as p on te.projeto_pnumero = p.pnumero
+	inner join departamento as d on p.dnumero = d.dnumero
+	where te.funcionario_cpf = f.cpf and d.dnome = 'Pesquisa'
+)
+and 
+exists (
+	select *
+    from dependente as dep
+    where dep.f_cpf = f.cpf
+)
+and 
+f.salario > (
+	select avg(salario)
+    from funcionario
+);
 
+-- 14
+# A difenrença entre WHERE e HAVING é que o where define condições para linhas únicas já o having define para grupos feitos pelo GROUP BY
+# Exemplo prático: para o WHERE a atividade 5 desta atividade e para o HAVING  a atividade 10 desta atividade
 
+-- 15
+#INNER  JOIN: linhas correlacionadas entre as tabelas
+-- Exemplo: retornar funcionários que estão trabalhando em um projeto
 
+#LEFT OUTER JOIN: apresenta todas a linhas da tabela refenrenciada na esquerda, mesmo não tendo corresponências
+-- Exemplo: Todos os funcionários com ou sem departamento
 
+# FULL OUTER JOIN: apresenta todas as linhas das duas tabela, mesmo não tendo correspondência
+-- Exemplo: Todos os funcionários e seus dependentes, mostrando null onde não tem correspondência
 
+-- 16
+# Uma situação que seria melhor usar o exist ao invés do in seria para realizar consultas muitas extensas, pois o exists apenas com uma consulta sendo true ele já para de realizar a varredura entre as tabelas
+-- Exemplo: listar os nomes dos departamentos que possuem pelo menos um funcionário.
 
 
